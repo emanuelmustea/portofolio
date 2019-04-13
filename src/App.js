6d3af37e431d8e7dceaae483a4780422e76984a6
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-04-14 00:35:17 +0300

    added header and menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header"
 
 class App extends Component {
   render() {
     return (
-      <div>
-       jgugiugu
+      <div className="App">
+       <Header />
       </div>
     );
   }
