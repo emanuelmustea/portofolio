895525e782cd4f5eff133721b6f596411d15d1dd
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-04-14 23:12:56 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./Header/Header"
-
+import Homepage from "./Homepage/Homepage"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App flex column">
        <Header />
+       <Homepage />
       </div>
     );
   }
---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Homepage.css"
+
+export default class Homepage extends Component{
+    render(){
+        return(<div className="flex column grow-full">
+            <div className="v-center">
+              <div className="logo text huge center">Emanuel Mustea</div>
+            </div>
+        </div>)
+    }
+}
