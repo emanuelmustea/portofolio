ac2bf82a59abce88d4066ed574a0760fda3cd33b
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-04-14 23:19:04 +0300

    dd

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -4,7 +4,8 @@ import "./Homepage.css"
 export default class Homepage extends Component{
     render(){
         return(<div className="flex column grow-full">
-            <div className="v-center">
+            <div className="v-center center">
+                <img src="/lab.svg" width="56px" />
               <div className="logo text huge center">Emanuel Mustea</div>
             </div>
         </div>)
